42681980dddc0d1adbb0d164de8663050ce0954a
hungqng <97866339+hungqng@users.noreply.github.com>
2022-01-26 13:53:18 -0800

    Add files via upload
    Documentation

--- explore.js
+++ explore.js
@@ -1,7 +1,11 @@
+// Install twilio package
 const Twilio = require("twilio");
 
+// Input SID and Authorized token
 const Client = new Twilio("Account SID", "Auth Token");
 
+// Display the most recent message
+// Also catch any error
 Client.messages.list().then(messages => console.log(`The most recent message is ${messages[0].body}`)).catch(err => console.error(err));
 
 console.log('Gathering your message log');

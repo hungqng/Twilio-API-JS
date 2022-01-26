6cf1a2acab3f559bab6ea5b6c5595924cb13dd92
hungqng <97866339+hungqng@users.noreply.github.com>
2022-01-26 13:50:52 -0800

    Add files via upload
    Add catch error to the function

--- explore.js
+++ explore.js
@@ -1,7 +1,7 @@
 const Twilio = require("twilio");
 
-const Client = new Twilio("SID", "Auth Token");
+const Client = new Twilio("Account SID", "Auth Token");
 
-Client.messages.list().then(messages => console.log(`The most recent message is ${message[0].body}`));
+Client.messages.list().then(messages => console.log(`The most recent message is ${messages[0].body}`)).catch(err => console.error(err));
 
 console.log('Gathering your message log');

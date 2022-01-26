3c317786cec29ae4038f71e4aa2f1ac48156901f
hungqng <97866339+hungqng@users.noreply.github.com>
2022-01-26 15:41:24 -0800

    Add files via upload

--- explore.js
+++ explore.js
@@ -1,11 +1,24 @@
+require('key.env').config
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+
 // Install twilio package
-const Twilio = require("twilio");
+const client = require("twilio")(accountSid, authToken);
 
 // Input SID and Authorized token
-const Client = new Twilio("Account SID", "Auth Token");
+// const Client = new Twilio("Account SID", "Auth Token");
 
 // Display the most recent message
 // Also catch any error
-Client.messages.list().then(messages => console.log(`The most recent message is ${messages[0].body}`)).catch(err => console.error(err));
+client.messages.list().then(messages => console.log(`The most recent message is ${messages[0].body}`)).catch(err => console.error(err));
 
 console.log('Gathering your message log');
+
+client.messages
+    .create({
+        to: '+12067246799',
+        from: '+16065311900',
+        body: 'Hello World from Twilio using JavaScript',
+    })
+    .then(messages => console.log(messages.sid));
